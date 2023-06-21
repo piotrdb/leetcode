c0166e86a6f05a1eda0902df04a25af02e0a6c3b
Piotr Dębski <piotrdb1@gmail.com>
2023-06-21 22:09:33 +0200

    temporary form for database injections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,38 @@
+import { useState } from 'react';
 import ProblemsTable from '../components/ProblemsTable/ProblemsTable';
 import TopBar from '../components/TopBar/TopBar';
+import { doc, setDoc } from 'firebase/firestore';
+import { firestore } from '../firebase/firebase';
 
 export default function Home() {
+  const [inputs, setInput] = useState({
+    id: '',
+    title: '',
+    difficulty: '',
+    category: '',
+    videoId: '',
+    link: '',
+    order: 0,
+    likes: 0,
+    dislikes: 0,
+  });
+
+  const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('submited');
+    await setDoc(doc(firestore, 'problems', inputs.id), {
+      ...inputs,
+      order: Number(inputs.order),
+    });
+  };
+
   return (
     <main className="bg-dark-layer-2 min-h-screen">
       <TopBar />
@@ -22,7 +53,6 @@ export default function Home() {
               <th scope="col" className="px-6 py-3 w-0 font-medium">
                 Difficulty
               </th>
-
               <th scope="col" className="px-6 py-3 w-0 font-medium">
                 Category
               </th>
@@ -34,6 +64,55 @@ export default function Home() {
           <ProblemsTable />
         </table>
       </div>
+
+      <form
+        className="p-6 flex flex-col max-w-sm gap-3"
+        onSubmit={handleSubmit}
+      >
+        <input
+          onChange={inputChange}
+          type="text"
+          placeholder="problem id"
+          name="id"
+        />
+        <input
+          onChange={inputChange}
+          type="text"
+          placeholder="title"
+          name="title"
+        />
+        <input
+          onChange={inputChange}
+          type="text"
+          placeholder="difficulty"
+          name="difficulty"
+        />
+        <input
+          onChange={inputChange}
+          type="text"
+          placeholder="category"
+          name="category"
+        />
+        <input
+          onChange={inputChange}
+          type="text"
+          placeholder="video id?"
+          name="videoId"
+        />
+        <input
+          onChange={inputChange}
+          type="text"
+          placeholder="video link?"
+          name="link"
+        />
+        <input
+          onChange={inputChange}
+          type="text"
+          placeholder="order"
+          name="order"
+        />
+        <button className="bg-white">save to database</button>
+      </form>
     </main>
   );
 }
